51b75ea04443933d58c27314dbb68ba5a24d232d
Aman <amanshankarsingh05@gmail.com>
2023-01-26 00:32:26 +0530

    few practice problems

---
+++ nextRound.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    int arr[n];
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= arr[k - 1] && arr[i] > 0)
+            cnt++;
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+while (n--)
+    // {
+    //     int a, b, c;
+    //     cin >> a >> b >> c;
+    //     int sum = a + b + c;
+    //     if (sum >= 2)
+    //         cnt++;
+    // }
