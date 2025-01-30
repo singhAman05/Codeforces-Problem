b11391a3afefa3da59c456d1d61f78a424b7542a
singhAman05 <amanshankarsingh05@gmail.com>
2025-01-31 00:39:14 +0530

    commiting files in main branch

---
+++ Subtract_min-sort.cpp
@@ -0,0 +1,103 @@
+// Problem: D. Subtract Min Sort
+// Contest: Codeforces - Codeforces Round 998 (Div. 3)
+// URL: https://codeforces.com/contest/2060/problem/D
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+typedef map<int, int> mpi;
+typedef set<int> seti;
+typedef unordered_map<int,int> uom;
+typedef unordered_map<char,int> uocm;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REV(i, a, b) for (int i = a; i >= b; i--)
+#define all(v) v.begin(), v.end()
+
+// GCD
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+// LCM
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+
+// Modular Exponentiation
+ll mod_exp(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
+}
+
+// Factorial of a Number;
+ll factorial(ll n) {
+    ll result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+// Prime Check
+bool is_prime(ll n) {
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+void fun(){
+    // your code
+    ll n;cin>>n;
+    vl v(n);
+    REP(i,0,n) cin>>v[i];
+    int j = 0;
+    REP(i,1,n){
+    	if(v[i-1] > v[i]) j = i;
+    }
+    
+    if(j==0) {cout<<"YES\n";return;}
+    
+    REP(i,0,j){
+    	ll mini = min(v[i],v[i+1]);
+    	v[i] -= mini;
+    	v[i+1] -= mini;
+    }
+    
+    cout<< (is_sorted(v.begin(),v.end()) ? "YES" : "NO")<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        fun();
+    }
+    return 0;
+}
