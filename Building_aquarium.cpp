71d079c2ff39e1c518e0c4e16458c64245561f0d
singhAman05 <amanshankarsingh05@gmail.com>
2025-06-08 22:50:17 +0530

    adding new questions

---
+++ Building_aquarium.cpp
@@ -0,0 +1,112 @@
+// Problem: E. Building an Aquarium
+// Contest: Codeforces - Codeforces Round 898 (Div. 4)
+// URL: https://codeforces.com/problemset/problem/1873/E
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+typedef map<int, int> mpi;
+typedef set<int> seti;
+typedef unordered_map<int,int> uom;
+typedef unordered_map<char,int> uocm;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REV(i, a, b) for (int i = a; i >= b; i--)
+#define all(v) v.begin(), v.end()
+#define nl "\n"
+
+// GCD
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+// LCM
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+
+// Modular Exponentiation
+ll mod_exp(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
+}
+
+// Factorial of a Number;
+ll factorial(ll n) {
+    ll result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+// Prime Check
+bool is_prime(ll n) {
+    if (n < 2) return false;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+ll solve(vector<ll> &v, ll mid){
+	ll cnt = 0;
+	for(ll i=0;i<v.size();i++){
+		cnt+= max(0LL, mid-v[i]);
+	}
+	return cnt;
+}
+
+void fun(){
+    // your code
+    ll n,w;cin>>n>>w;
+    vector<ll> v(n);
+    REP(i,0,n) cin>>v[i];
+    ll l=1, r=INT_MAX;
+    ll ans = 1;
+    while(l<=r){
+    	ll mid = l + (r-l)/2;
+    	if(solve(v,mid)>w){
+    		r=mid-1;
+    	}
+    	else {
+    		ans = mid;
+    		l=mid+1;
+    	}
+    }
+    
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        fun();
+    }
+    return 0;
+}
